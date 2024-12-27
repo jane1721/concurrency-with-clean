2a7cccb80229bdc960f02aec9c4d15a54df57705
jane <jane1721@gmail.com>
2024-12-27 12:28:37 +0900

    🎉Init: Create Spring Boot Project

---
+++ src/main/java/com/jane/concurrencywithclean/ConcurrencyWithCleanApplication.java
@@ -0,0 +1,13 @@
+package com.jane.concurrencywithclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyWithCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyWithCleanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jane/concurrencywithclean/ConcurrencyWithCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jane.concurrencywithclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyWithCleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
